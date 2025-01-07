3442d3cbb01435a73a3b316f2ddcb0af2a0170ec
Aadarsh Gupta <111424028+aadarshg25@users.noreply.github.com>
2025-01-07 12:15:54 +0530

    Add files via upload

--- MyCalculator.java
+++ MyCalculator.java
@@ -1,21 +1,45 @@
 import java.util.Scanner;
 
-public class Calculator {
+public class MyCalculator {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);2
-        int a = sc.nextInt();
-        String b = sc.next();
-        int c = sc.nextInt();
-        if (b == "+") {
-            System.out.println(a + c);
-        } else if (b == "-") {
-            System.out.println(a - c);
-        } else if (b == "*") {
-            System.out.println(a * c);
-        } else if (b == "/") {
-            System.out.println(a / c);
-        } else {
-            System.out.println("Enter a  Valid input");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("My Calculator");
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+        
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+        
+        double result;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error! Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+                return;
         }
+        
+        System.out.println("The result is: " + result);
+        scanner.close();
     }
 }
